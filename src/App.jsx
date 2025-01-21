2750dedc9fb80ac43024fd87bc1feff30b82dfa8
yykim <rladuddbs789@gmail.com>
2025-01-21 15:18:58 +0900

    chore: reset css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SampleComponent from './components/atoms/SampleComponent'
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>4조 수연 연습 </h1>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>4조 수연 연습 </h1>
+            <SampleComponent>샘플 컴포넌트</SampleComponent>
+        </>
+    )
 }
 
-export default App;
+export default App
