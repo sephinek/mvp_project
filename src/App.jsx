33f3d101d76514d21c10d1c58fdc8daf0c8c12f1
yykim <rladuddbs789@gmail.com>
2025-02-05 15:05:42 +0900

    chore: 라우트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-import SampleMolecules from './components/molecules/SampleMolecules'
+import Router from './shared/Router'
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>4조 수연 연습 </h1>
-            <SampleMolecules />
-        </>
-    )
+    return <Router />
 }
 
 export default App
