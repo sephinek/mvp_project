f65d6016162cb7803f2593fc3bcc158ba08841e5
yykim <rladuddbs789@gmail.com>
2025-02-06 16:28:03 +0900

    feat: 레이아웃 스타일 수정

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import ConfirmModal from '../Modal/ConfirmModal'
+import styles from './index.module.css'
 
 const Layout = () => {
     return (
-        <>
+        <div className={styles.layout}>
             <Outlet />
             <ConfirmModal />
-        </>
+        </div>
     )
 }
 
