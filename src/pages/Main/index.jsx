cb59c96c6855f97274b51672cbafbf2eb6344622
Sephine Kang <sephine.k31@gmail.com>
2025-02-11 20:21:44 +0900

    updated on main page

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,10 +1,12 @@
 import Plans from '../../components/molecules/Plans';
+import Vision from '../../components/molecules/Vision';
 import styles from './index.module.css';
 
 export default function Main() {
   return (
     <div>
       Main
+      <Vision />
       <Plans />
     </div>
   );
