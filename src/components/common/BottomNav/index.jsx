16b5cb7545645e6db0d2e8122a4afbc3efc1eb52
eunzi <eunzi789@gmail.com>
2025-02-13 17:02:17 +0900

    bottom NavBar

--- src/components/common/BottomNav/index.jsx
+++ src/components/common/BottomNav/index.jsx
@@ -12,11 +12,12 @@ export default function BottomNav() {
           <img className={styles.icon} />
           <span className={styles.menuTitle}>점검데이</span>
         </li>
-        <li className={styles.menuBox}></li> {/* 공간만 확보 */}
-        <div>
-          <img className={styles.iconAdd} />
-        </div>{' '}
-        {/* Absolute로 자리잡기 */}
+        <li className={styles.menuBox}>
+          {/* Absolute로 자리잡기 */}
+          <div>
+            <img className={styles.icon} />
+          </div>
+        </li>
         <li className={styles.menuBox}>
           <img className={styles.icon} />
           <span className={styles.menuTitle}>통계</span>
