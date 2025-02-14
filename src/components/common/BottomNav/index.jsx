bf2576936bdc3043648ec55517780970acc683e2
Sephine Kang <sephine.k31@gmail.com>
2025-02-14 14:38:11 +0900

    fixed bottom nav

--- src/components/common/BottomNav/index.jsx
+++ src/components/common/BottomNav/index.jsx
@@ -1,31 +1,79 @@
+import { useState } from 'react';
+import IconButton from '../../atoms/IconButton/index.jsx';
+import HomeIcon from '../../../assets/icons/solar/BottomNav-icon01.svg';
+import ReflectionDayIcon from '../../../assets/icons/solar/BottomNav-icon02.svg';
+import AddIcon from '../../../assets/icons/solar/BottomNav-icon03.svg';
+import StatisticsIcon from '../../../assets/icons/solar/BottomNav-icon04.svg';
+import MyPageIcon from '../../../assets/icons/solar/BottomNav-icon05.svg';
 import styles from './index.module.css';
+import { Link } from 'react-router-dom';
+
+const MENUS = [
+  { title: '홈', alt: 'Home menu', icon: HomeIcon, to: '/' },
+  {
+    title: '점검데이',
+    alt: 'Reflection Day menu',
+    icon: ReflectionDayIcon,
+    to: '/reflection',
+  },
+  { title: '추가', alt: 'Add menu', icon: AddIcon },
+  {
+    title: '통계',
+    alt: 'Statistics menu',
+    icon: StatisticsIcon,
+    to: '/statistics',
+  },
+  { title: 'MY', alt: 'My Page menu', icon: MyPageIcon, to: '/mypage' },
+];
 
 export default function BottomNav() {
+  const [currentMenu, setCurrentMenu] = useState(MENUS[0]);
+
+  const clickHandler = (menu) => {
+    setCurrentMenu(menu);
+  };
+
+  console.log(currentMenu);
   return (
     <nav className={styles.bottomNavContainer}>
       <ul className={styles.bottomNavBox}>
-        <li className={styles.menuBox}>
-          <img className={styles.icon} />
-          <span className={styles.menuTitle}>홈</span>
-        </li>
-        <li className={styles.menuBox}>
-          <img className={styles.icon} />
-          <span className={styles.menuTitle}>점검데이</span>
-        </li>
-        <li className={styles.menuBox}>
-          {/* Absolute로 자리잡기 */}
-          <div>
-            <img className={styles.icon} />
-          </div>
-        </li>
-        <li className={styles.menuBox}>
-          <img className={styles.icon} />
-          <span className={styles.menuTitle}>통계</span>
-        </li>
-        <li className={styles.menuBox}>
-          <img className={styles.icon} />
-          <span className={styles.menuTitle}>MY</span>
-        </li>
+        {MENUS.map((menu) => {
+          if (menu.title !== '추가') {
+            return (
+              <Link key={menu.title} className={`${styles.menuBox}`}>
+                <IconButton
+                  icon={menu.icon}
+                  alt={menu.alt}
+                  type='bottomNavIcon'
+                  className={styles.icon}
+                  onClick={() => clickHandler(menu)}
+                  to={menu.to}
+                />
+                <span
+                  className={`${styles.menuTitle} ${
+                    menu === currentMenu ? 'styles.active' : ''
+                  }`}
+                >
+                  {menu.title}
+                </span>
+              </Link>
+            );
+          } else {
+            return (
+              <li key={menu.title} className={styles.menuBox}>
+                <div>
+                  <IconButton
+                    icon={AddIcon}
+                    alt={menu.alt}
+                    type='bottomNavAddIcon'
+                    active={false}
+                    className={styles.icon}
+                  />
+                </div>
+              </li>
+            );
+          }
+        })}
       </ul>
     </nav>
   );
--- src/components/common/Modal/ConfirmModal/index.jsx
+++ src/components/common/Modal/ConfirmModal/index.jsx
@@ -1,21 +1,27 @@
-import Modal from 'react-modal'
+import Modal from 'react-modal';
 
-import { useRecoilState } from 'recoil'
-import { confirmModalState } from '../../../../shared/recoil/confirmModalState'
+import { useRecoilState } from 'recoil';
+import { confirmModalState } from '../../../../shared/recoil/confirmModalState';
 
 const ConfirmModal = () => {
-    const [modalOpen, setModalOpen] = useRecoilState(confirmModalState)
+  const [modalOpen, setModalOpen] = useRecoilState(confirmModalState);
 
-    const handleClose = () => {
-        setModalOpen({ isOpen: false })
-    }
+  const handleClose = () => {
+    setModalOpen({ isOpen: false });
+  };
 
-    return (
-        <Modal isOpen={modalOpen.isOpen} onRequestClose={handleClose} ariaHideApp={false} contentLabel="Pop up Message" shouldCloseOnOverlayClick={false}>
-            <div>준비중입니다.</div>
-            <button onClick={handleClose}>확인</button>
-        </Modal>
-    )
-}
+  return (
+    <Modal
+      isOpen={modalOpen.isOpen}
+      onRequestClose={handleClose}
+      ariaHideApp={false}
+      contentLabel='Pop up Message'
+      shouldCloseOnOverlayClick={false}
+    >
+      <div>준비중입니다.</div>
+      <button onClick={handleClose}>확인</button>
+    </Modal>
+  );
+};
 
-export default ConfirmModal
+export default ConfirmModal;
