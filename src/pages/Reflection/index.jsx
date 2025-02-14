e1dc1063946caf9e17d2037c4ac3c8a343aa2980
eunzi <eunzi789@gmail.com>
2025-02-14 17:50:57 +0900

    점검데이 empty-state 컴포넌트 생성

--- src/pages/Reflection/index.jsx
+++ src/pages/Reflection/index.jsx
@@ -1,16 +1,19 @@
+import ReflectionEmptyState from '../../components/atoms/ReflectionEmptyState';
 import ReflectionPlusButton from '../../components/atoms/ReflectionPlusButton';
 import ReflectionTab from '../../components/atoms/ReflectionTab';
 import ReflectionTopBar from '../../components/atoms/ReflectionTopBar';
 import useNavigationPage from '../../hooks/useNavigationPage';
+import styles from './index.module.css';
 
 export default function Reflection() {
   const { state, routePage } = useNavigationPage();
 
   return (
     <>
-      <div>
+      <div className={styles.reflectionPage}>
         <ReflectionTopBar>점검데이</ReflectionTopBar>
         <ReflectionTab></ReflectionTab>
+        <ReflectionEmptyState></ReflectionEmptyState>
         <ReflectionPlusButton></ReflectionPlusButton>
       </div>
     </>
