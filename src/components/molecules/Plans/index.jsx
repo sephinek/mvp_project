f1a8f5b5e299c844f66ff32b6ce0b1b47dab11e1
whatthebbo <whatthebbo@gmail.com>
2025-02-14 22:47:54 +0900

    fix/plans: currentDate 포맷 함수 제거

--- src/components/molecules/Plans/index.jsx
+++ src/components/molecules/Plans/index.jsx
@@ -3,14 +3,12 @@ import SectionTitle from '../../atoms/SectionTitle/index.jsx';
 import PlansCalender from '../PlansCalendar.jsx/index.jsx';
 import PlansList from '../PlansList/index.jsx';
 import { useEffect, useState } from 'react';
-import { format } from 'date-fns';
 
 export default function Plans({ goals }) {
   const [currentDate, setCurrentDate] = useState(
-    format(new Date(), 'yyyy-MM-dd HH:mm:ss.SSS')
+    new Date()
   );
   const [plans, setPlans] = useState([]);
-
 
   const handleCurrentDate = (date) => {
     // setCurrentDate(format(date, 'yyyy-MM-dd HH:mm:ss.SSS'));
@@ -22,15 +20,6 @@ export default function Plans({ goals }) {
       const result = [];
       goals?.map((goal) => {
         goal?.plans?.map((plan) => {
-          console.log(
-            'currentDate',
-            currentDate,
-            '***',
-            typeof plan.startDate,
-            '***',
-            plan.endDate,
-            '***'
-          );
           if (plan.endDate >= currentDate && plan.startDate <= currentDate) {
             result.push(plan);
           }
@@ -41,7 +30,7 @@ export default function Plans({ goals }) {
     };
 
     getPlans();
-  }, [currentDate]);
+  }, [currentDate, goals]);
 
   console.log(goals, plans);
 
