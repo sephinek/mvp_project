95a6b365dd35c7eb3cdd3ed0850639e708c28250
eunzi <eunzi789@gmail.com>
2025-02-15 12:47:36 +0900

    통계페이지 메인 마듬

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -17,6 +17,8 @@ export default function Reflection() {
         <StatisticsProfile></StatisticsProfile>
         <div className={styles.statisticsWrap}>
           <StatisticsStatusCard></StatisticsStatusCard>
+
+          {/* 여기 자체를 컴포넌트로 만들어도 될 듯*/}
           <div className={styles.contentBox}>
             <StatisticsPeriodTabs></StatisticsPeriodTabs>
             <span></span>
@@ -32,6 +34,65 @@ export default function Reflection() {
             <div className={styles.graph}></div>
             <span></span>
             <StatisticsPlanList></StatisticsPlanList>
+          </div>
+          
+          <div className={styles.contentBox}>
+            <div className={styles.goalWrap}>
+              <ul className={styles.goalsList}>
+                
+                {/* 복사해온 코드*/}
+                <li className={styles.goalBox}>
+                  <div className={styles.titleBox}>
+                    <span className={styles.title}>창의적 활동 지속하기</span>
+                  </div>
+                  <div>
+                    <div className={styles.percentageAndDueDate}>
+                      <span className={styles.percentage}>30%</span>
+                      <span className={styles.dueDate}>01/01/2026</span>
+                    </div>
+                    <div className={styles.bar}>
+                      <div className={styles.full}>
+                        <div className={styles.filled}></div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li className={styles.goalBox}>
+                  <div className={styles.titleBox}>
+                    <span className={styles.title}>창의적 활동 지속하기</span>
+                  </div>
+                  <div>
+                    <div className={styles.percentageAndDueDate}>
+                      <span className={styles.percentage}>30%</span>
+                      <span className={styles.dueDate}>01/01/2026</span>
+                    </div>
+                    <div className={styles.bar}>
+                      <div className={styles.full}>
+                        <div className={styles.filled}></div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li className={styles.goalBox}>
+                  <div className={styles.titleBox}>
+                    <span className={styles.title}>창의적 활동 지속하기</span>
+                  </div>
+                  <div>
+                    <div className={styles.percentageAndDueDate}>
+                      <span className={styles.percentage}>30%</span>
+                      <span className={styles.dueDate}>01/01/2026</span>
+                    </div>
+                    <div className={styles.bar}>
+                      <div className={styles.full}>
+                        <div className={styles.filled}></div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
 
         </div>
