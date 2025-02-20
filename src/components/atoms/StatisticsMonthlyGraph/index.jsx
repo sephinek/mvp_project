f7de9a3e594ca1df547a358278e97cf1146e7bb5
eunzi <eunzi789@gmail.com>
2025-02-20 17:19:15 +0900

    자세히 보기 버튼 추가

---
+++ src/components/atoms/StatisticsMonthlyGraph/index.jsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+
+const StatisticsMonthlyGraph = () => {
+
+    return (
+        <div className={styles.statisticsGraph}>
+                d
+        </div>
+    );
+}
+
+export default StatisticsMonthlyGraph;
