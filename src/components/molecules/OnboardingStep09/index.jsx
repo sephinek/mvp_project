7a8b4495917e5b1a36cabb4c8815e36085444a3b
Sephine Kang <sephine.k31@gmail.com>
2025-02-21 17:46:34 +0900

    modified ai

--- src/components/molecules/OnboardingStep09/index.jsx
+++ src/components/molecules/OnboardingStep09/index.jsx
@@ -1,18 +1,24 @@
 import { useEffect, useRef, useState } from 'react';
+import { ClimbingBoxLoader } from 'react-spinners';
 import Button from '../../atoms/Button';
 import GoBackHeader from '../../common/GoBackHeader';
 import OnBoardingTitle from '../../atoms/OnboardingTitle';
-import styles from './index.module.css';
 import Input from '../../atoms/Input';
-import OnboardingYesIcon from '../../../assets/icons/toss/yes.svg';
 import OnboardingCheckList from '../OnboardingCheckList';
 import AiIcon from '../../../assets/icons/humble/ai.svg';
-import AiActiveIcon from '../../../assets/icons/humble/ai-active.svg';
+
+import styles from './index.module.css';
 
 const OnboardingStep09 = ({ nextStep, beforeStep, type }) => {
   const ref = useRef(null);
   const [isConfirmed, setIsConfirmed] = useState(false);
   const [target, setTarget] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 3500);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -57,6 +63,7 @@ ex) 매일 30분씩 영어 공부하기'
       </div>
     );
   }
+
   return (
     <div className={styles.wrap}>
       <div className={styles.goBackBox}>
@@ -70,40 +77,51 @@ ex) 매일 30분씩 영어 공부하기'
           어떤 계획을 세워야 할까요?
         </OnBoardingTitle>
 
-        <div className={styles.button_wrap}>
-          <OnboardingCheckList
-            name='step01'
-            choice='single'
-            items={[
-              {
-                value: '창의적인 활동을 지속적으로 이어가기',
-                children: '창의적인 활동을 지속적으로 이어가기',
-                targetFn: () =>
-                  setTarget('창의적인 활동을 지속적으로 이어가기'),
-              },
-              {
-                value: '매달 새로운 지식이나 기술 배우기',
-                children: '매달 새로운 지식이나 기술 배우기',
-                targetFn: () => setTarget('매달 새로운 지식이나 기술 배우기'),
-              },
-              {
-                value: '건강한 생활 습관을 유지하기',
-                children: '건강한 생활 습관을 유지하기',
-                targetFn: () => setTarget('건강한 생활 습관을 유지하기'),
-              },
-              {
-                value: '재정적인 자유 달성하기',
-                children: '재정적인 자유 달성하기',
-                targetFn: () => setTarget('재정적인 자유 달성하기'),
-              },
-              {
-                value: '사회적인 네트워크 구축하기',
-                children: '사회적인 네트워크 구축하기',
-                targetFn: () => setTarget('사회적인 네트워크 구축하기'),
-              },
-            ]}
-          />
-        </div>
+        {isLoading ? (
+          <div className={styles.loader}>
+            <ClimbingBoxLoader color='#ff1b1a' />
+          </div>
+        ) : (
+          <div className={styles.button_wrap}>
+            <OnboardingCheckList
+              name='step01'
+              choice='single'
+              items={[
+                {
+                  value: '창의적인 활동을 지속적으로 이어가기',
+                  children: '창의적인 활동을 지속적으로 이어가기',
+                  targetFn: () =>
+                    setTarget('창의적인 활동을 지속적으로 이어가기'),
+                },
+                {
+                  value: '매달 새로운 지식이나 기술 배우기',
+                  children: '매달 새로운 지식이나 기술 배우기',
+                  targetFn: () => setTarget('매달 새로운 지식이나 기술 배우기'),
+                },
+                {
+                  value: '건강한 생활 습관을 유지하기',
+                  children: '건강한 생활 습관을 유지하기',
+                  targetFn: () => setTarget('건강한 생활 습관을 유지하기'),
+                },
+                {
+                  value: '재정적인 자유 달성하기',
+                  children: '재정적인 자유 달성하기',
+                  targetFn: () => setTarget('재정적인 자유 달성하기'),
+                },
+                {
+                  value: '사회적인 네트워크 구축하기',
+                  children: '사회적인 네트워크 구축하기',
+                  targetFn: () => setTarget('사회적인 네트워크 구축하기'),
+                },
+              ]}
+            />
+
+            <button className={styles.aiBtn}>
+              <img src={AiIcon} className={styles.aiIcon} />
+              <span className={styles.aiText}>다시 생성하기</span>
+            </button>
+          </div>
+        )}
       </div>
 
       <div className={styles.bottomBtn}>
