77ed3c935e62f75b88c6e68d77c1ab8c93bfff6e
whatthebbo <whatthebbo@gmail.com>
2025-02-21 21:28:43 +0900

    fix/main.plan: get plan

--- src/components/molecules/Plans/index.jsx
+++ src/components/molecules/Plans/index.jsx
@@ -2,36 +2,23 @@ import styles from './index.module.css';
 import SectionTitle from '../../atoms/SectionTitle/index.jsx';
 import PlansCalender from '../PlansCalendar.jsx/index.jsx';
 import PlansList from '../PlansList/index.jsx';
-import { useEffect, useState } from 'react';
-import { getDate } from '../../../utils/date.js';
+import { useMemo, useState } from 'react';
+import { addDays, isAfter, isBefore } from 'date-fns';
 
 export default function Plans({ goals, onPlanClick }) {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [plans, setPlans] = useState([]);
-
+  
   const handleCurrentDate = (date) => {
-    setCurrentDate(new Date(date));
+    setCurrentDate(date);
   };
 
-  useEffect(() => {
-    const getPlans = () => {
-      const result = [];
-      goals?.forEach((goal) => {
-        goal?.plans?.forEach((plan) => {
-          if (
-            getDate(plan.endDate) >= getDate(currentDate) &&
-            getDate(plan.startDate) <= getDate(currentDate)
-          ) {
-            result.push({ ...plan, goalId: goal.id, goalTitle: goal.title });
-          }
-        });
-      });
-
-      setPlans(result);
-    };
-
-    getPlans();
-  }, [currentDate, goals]);
+  const plans = useMemo(() => {
+    return goals.flatMap(({plans}) => plans).filter(({
+      startDate,
+      endDate,
+      repetition,
+    }) => isAfter(currentDate, addDays(startDate, -1)) && isBefore(currentDate, endDate) && repetition.includes(currentDate.getDay()));
+  }, [goals, currentDate])
 
   return (
     <section className={styles.planContainer}>
