f82b7e2093e48aa943e3d3f55b38dc04f8e09b0c
whatthebbo <whatthebbo@gmail.com>
2025-02-21 20:40:36 +0900

    fix/vision-board: attach file

--- src/pages/VisionBoard/index.jsx
+++ src/pages/VisionBoard/index.jsx
@@ -1,13 +1,22 @@
-import ReflectionEmptyState from '../../components/atoms/ReflectionEmptyState';
+import { useEffect, useState } from 'react';
 import ReflectionVisionBoard from '../../components/atoms/ReflectionVisionBoard';
-import ReflectionPlusButton from '../../components/atoms/ReflectionPlusButton';
+import styles from './index.module.css';
 
 export default function VisionBoard() {
+  const [images, setImages] = useState([]);
+
+  const handleFileChange = (e) => {
+    const { target :{files} } = e;
+    setImages(prev => [...prev, ...Array.from(files).map(file => URL.createObjectURL(file))]);
+  }
+
   return (
     <>
       {/* <ReflectionEmptyState></ReflectionEmptyState> */}
-      <ReflectionVisionBoard></ReflectionVisionBoard>
-      <ReflectionPlusButton></ReflectionPlusButton>
+      <ReflectionVisionBoard images={images} />
+      <label htmlFor="fileInput" className={styles['file-input-label']} aria-label='이미지 첨부'>
+      </label>
+      <input type="file" name="fileInput" id="fileInput" className={styles['file-input']} accept='image/*' onChange={handleFileChange} />
     </>
   );
 }
