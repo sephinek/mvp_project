1c9014ec81f76e0ddb74aab6aad9f8d27ad41eb5
eunzi <eunzi789@gmail.com>
2025-02-24 02:24:59 +0900

    AI버튼 생성 중 상태 작업

--- src/components/molecules/Ai_button/index.jsx
+++ src/components/molecules/Ai_button/index.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState} from "react";
 import styles from './index.module.css';
 
 const Ai_button = ({
-  title, // 타이틀 텍스트
-  onClick, // 버튼 클릭 이벤트 핸들러
+  title, 
+  onClick, 
 }) => {
+
+  const [isSetting, setIsSetting] = useState(false); 
+  const [hasGenerated, setHasGenerated] = useState(false); 
+
+  const handleClick = async () => {
+    if (!hasGenerated) {
+      setIsSetting(true);
+      try {
+        // 로딩 중 상태
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        setIsSetting(false); 
+        setHasGenerated(true); 
+      } catch (error) {
+        setIsSetting(false);
+      }
+    } else {
+      // '다시 생성하기' 클릭 시, AI 다시 생성
+      setHasGenerated(false);
+      setIsSetting(false);
+    }
+    
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
     <div className={styles.card}>
-      {/* 텍스트 영역 */}
       <div className={styles.textArea}>
-        {/* 아이콘 영역 */}
         <div className={styles.icon}></div>
 
-        {/* 타이틀과 서브타이틀 */}
         <div className={styles.textContent}>
           {title && <div className={styles.title}>{title}</div>}
           <div className={styles.subtitle}>해당 기능은 Chat GPT를 활용합니다.<br></br>개인정보 입력은 유의해 주세요</div>
         </div>
       </div>
-
-      {/* 버튼 영역 */}
-      <button className={styles.button} onClick={onClick}>
+      <button 
+        className={styles.button} 
+        onClick={handleClick}
+        disabled={isSetting}
+      >
           <div className={styles.buttonIcon}></div>
-          <span className={styles.buttonText}>AI 계획 추천받기</span>
+          <span className={styles.buttonText}>
+          {isSetting ? "AI 세팅중입니다..." : hasGenerated ? "다시 생성하기" : "AI 계획 추천받기"}</span>
       </button>
     </div>
   );
