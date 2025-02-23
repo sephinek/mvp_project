1afa8f4394f948cd55c7dbe2ce4d94dd122f0de9
eunzi <eunzi789@gmail.com>
2025-02-23 17:29:09 +0900

    마이페이지 상단카드  컴포넌트 생성

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,11 +1,13 @@
 import myPageImg from '../../assets/image/mypage.png';
 import BottomNav from '../../components/common/BottomNav';
 import styles from './index.module.css';
+import MyPageOverview from '../../components/atoms/myPageOverview';
 
 export default function MyPage() {
   return (
     <section className={styles.imgContainer}>
-      <img className={styles.img} src={myPageImg} alt='My Page Mock' />;
+      <MyPageOverview></MyPageOverview>
+      {/* <img className={styles.img} src={myPageImg} alt='My Page Mock' />; */}
       <BottomNav />
     </section>
   );
