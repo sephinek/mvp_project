d1b25d746090db037eca314f31408581d0066f81
whatthebbo <whatthebbo@gmail.com>
2025-02-24 21:25:38 +0900

    f

--- src/components/atoms/ReflectionTab/index.jsx
+++ src/components/atoms/ReflectionTab/index.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react';
+import {  useLayoutEffect, useState } from 'react';
 import styles from './index.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+const tabs = [
+  { title: '비전보드', to: '/reflection/vision-board' },
+  { title: '회고', to: '/reflection/review' },
+  { title: '가이드', to: '/reflection/guide' },
+  { title: '계획정리', to: '/reflection/plans-review' },
+];
 
 const ReflectionTab = ({ children, ...rest }) => {
   const [activeTab, setActiveTab] = useState(0); // 활성화된 탭 상태
+  const location = useLocation();
 
   // 탭 이름 배열
   // const tabNames = ['비전보드', '회고', '가이드', '계획정리'];
-  const tabs = [
-    { title: '비전보드', to: '/reflection/vision-board' },
-    { title: '회고', to: '/reflection/review' },
-    { title: '가이드', to: '/reflection/guide' },
-    { title: '계획정리', to: '/reflection/plans-review' },
-  ];
+
 
   // 탭 클릭 시 활성화된 탭 변경
   const handleTabClick = (index) => {
     setActiveTab(index);
   };
+
+  useLayoutEffect(() => {
+    if(!location) return;
+    const { pathname } = location;
+    const index = tabs.findIndex(({to}) => to === pathname);
+    setActiveTab(index);
+  }, [
+    location
+  ])
 
   return (
     <div {...rest} className={styles.tabContainer}>
